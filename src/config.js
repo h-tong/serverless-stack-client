af87c2deaf5d864f098e0f65c99d36150e2a75b5
Howard Tong <howardtong@BDS-C02XQ51VJG5J.local>
2020-05-11 13:21:30 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "ht-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://j4uxsb1v36.execute-api.us-west-2.amazonaws.com/howard"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_F3Hn83hJd",
+    APP_CLIENT_ID: "603o29p3d4eqgvhajopsauaipj",
+    IDENTITY_POOL_ID: "us-west-2:51b02a49-45e3-4ab8-a585-fc3b82455c19"
+  }
+};
